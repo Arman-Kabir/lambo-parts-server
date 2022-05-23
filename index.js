2c7f3929c0e3e7dc25af12b7b52b693d292e4180
Arman-Kabir <armankabir8@gmail.com>
2022-05-23 13:06:30 +0600

    db connected and parts api done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,12 +9,42 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ct9v9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log('db connected');
+
+async function run() {
+    try {
+        await client.connect();
+        // console.log('db connected 2nd')
+        const partsCollection = client.db('lambo-parts').collection('parts');
+
+
+
+        // console.log(partsCollection);
+
+        // get all parts
+        app.get('/parts', async (req, res) => {
+            // const query = {};
+            const parts = await partsCollection.find().toArray();
+            res.send(parts);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
