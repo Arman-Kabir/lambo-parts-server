16709dcdcd9a1dcba08916a591ddaf670bc8e51f
Arman-Kabir <armankabir8@gmail.com>
2022-05-24 19:54:57 +0600

    verify jwt done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
@@ -13,16 +14,53 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log('db connected');
 
+
+function verifyJWT(req, res, next) {
+    // console.log('abc');
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        // console.log(decoded.foo) // bar
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+
 async function run() {
     try {
         await client.connect();
         // console.log('db connected 2nd')
         const partsCollection = client.db('lambo-parts').collection('parts');
         const orderCollection = client.db('lambo-parts').collection('orders');
+        const userCollection = client.db('lambo-parts').collection('users');
 
 
 
         // console.log(partsCollection);
+
+        // put user api
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            // token creation
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ result, token });
+        })
 
 
         // get all parts
@@ -40,12 +78,20 @@ async function run() {
             res.send(singleParts);
 
         })
-        // get orders of a specific user
-        app.get('/orders', async (req, res) => {
+        // get orders of a specific user // 
+        app.get('/orders', verifyJWT, async (req, res) => {
             const user = req.query.userEmail;
-            const query = {userEmail:user};
-            const orders= await orderCollection.find(query).toArray();
-            res.send(orders);
+            const decodedEmail = req.decoded.email;
+
+            if (user === decodedEmail) {
+                const query = { userEmail: user };
+                const orders = await orderCollection.find(query).toArray();
+                return res.send(orders);
+            }
+            else {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+
         })
         // order parts
         app.post('/orders', async (req, res) => {
